f507425bf4459626838505bd870b3ce774d9d1a1
Samolyb <artemte6575@gmail.com>
2024-08-09 18:07:58 +0100

    update-operations-url

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const baseURL = 'https://connections-api.goit.global/';
-
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchContacts',
     async (_, thunkAPI) => {
         try {
-            const response = await axios.get(`${baseURL}/contacts`);
+            const response = await axios.get(`/contacts`);
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +17,7 @@ export const addContact = createAsyncThunk(
     'contacts/addContact',
     async (contact, thunkAPI) => {
         try {
-            const response = await axios.post(`${baseURL}/contacts`, contact);
+            const response = await axios.post(`/contacts`, contact);
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +29,7 @@ export const deleteContact = createAsyncThunk(
     'contacts/deleteContact',
     async (contactId, thunkAPI) => {
         try {
-            const response = await axios.delete(`${baseURL}/contacts/${contactId}`);
+            const response = await axios.delete(`/contacts/${contactId}`);
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
